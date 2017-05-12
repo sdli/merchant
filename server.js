10e1ae1b62e02025fec550bb37dc508c44ed592c
steven <steven@ydongj.com>
2017-05-12 15:56:08 +0800

    steven

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var app = new (express)();
+
+app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, '/dist/static')));
+
+app.listen(8000,function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Server is listening on port %s. Open up http://localhost:%s/ in your browser.", 8000, 8000)
+  }
+});
